d974210c5002577cd1dcae1ee42de991b746156a
Sagar Vaghela <53026972+sagarvaghela4041@users.noreply.github.com>
2021-05-31 12:31:54 +0530

    temp commit

--- src/constants/messages.ts
+++
@@ -1,7 +0,0 @@
-
-export const messages = {
-    invalid_credentailss: 'Invalid Credentials !',
-    valid_user: 'Valid user ! Welcome.',
-    token_not_matched: 'token not matched !',
-    category_not_found: 'Category not found with provided id !'
-};
--- src/middlewares/error-handler.ts
+++
@@ -1,6 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-
-export function errorHandler (err:Error, req:Request, res:Response, next:NextFunction) {
-    res.json({ error: err.message })
-  }
